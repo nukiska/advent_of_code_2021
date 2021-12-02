e3979946b64e34b5bd9476c98e059fde7eeaaf06
nukiska <g.nina@seznam.cz>
2021-12-02 22:13:51 +0100

    added Day 1 code

---
+++ day_01/day_01.py
@@ -0,0 +1,34 @@
+input_list = []
+with open('day_01_input.txt', encoding='utf-8') as f:
+    for line in f:
+        input_list.append(int(line.rstrip()))
+
+
+def count_increase(data) -> int:
+    increased = 0
+    previous_value = 0
+    for i in range(len(data)):
+        if previous_value == 0:
+            previous_value = data[i]
+            continue
+        elif data[i] > previous_value:
+            increased += 1
+        previous_value = data[i]
+    return increased
+
+
+def get_triplets(data) -> list:
+    data_list = []
+    try:
+        for i in range(len(data)):
+            data_list.append(data[i] + data[i + 1] + data[i + 2])
+    except IndexError:
+        return data_list
+
+
+if __name__ == '__main__':
+    # task 1
+    print(count_increase(input_list))
+    # task 2
+    triplets_list = get_triplets(input_list)
+    print(count_increase(triplets_list))
